021241d95f94b558cfd7074182be186f1d247845
Vayruz Rafli <vayruzrafli79@outlook.co.id>
2023-06-27 05:54:49 +0700

    lime: libinit: Match fingerprint with proprietary blobs
    Change-Id: I00b7915d48426f338641b9b36d9fe7818bc78995

--- libinit/init_xiaomi_lime.cpp
+++ libinit/init_xiaomi_lime.cpp
@@ -17,7 +17,7 @@ static const variant_info_t lime_info = {
     .device = "lime",
     .marketname = "Redmi 9T",
     .model = "M2010J19SG",
-    .build_fingerprint = "Redmi/lime_global/lime:12/SKQ1.211202.001/V13.0.2.0.SJQMIXM:user/release-keys",
+    .build_fingerprint = "Redmi/lime_global/lime:12/SKQ1.211202.001/V13.0.3.0.SJQMIXM:user/release-keys",
 };
 
 static const variant_info_t pomelo_info = {
@@ -28,7 +28,7 @@ static const variant_info_t pomelo_info = {
     .device = "pomelo",
     .marketname = "Redmi 9 Power",
     .model = "M2010J19SL",
-    .build_fingerprint = "Redmi/pomelo_global/pomelo:12/SKQ1.211202.001/V13.0.2.0.SJQMIXM:user/release-keys",
+    .build_fingerprint = "Redmi/pomelo_global/pomelo:12/SKQ1.211202.001/V13.0.3.0.SJQMIXM:user/release-keys",
 };
 
 static const variant_info_t lemon_info = {
@@ -39,7 +39,7 @@ static const variant_info_t lemon_info = {
     .device = "lemon",
     .marketname = "Redmi 9T NFC",
     .model = "M2010J19SY",
-    .build_fingerprint = "Redmi/lemon_global/lemon:12/SKQ1.211202.001/V13.0.2.0.SJQMIXM:user/release-keys",
+    .build_fingerprint = "Redmi/lemon_global/lemon:12/SKQ1.211202.001/V13.0.3.0.SJQMIXM:user/release-keys",
 };
 
 static const std::vector<variant_info_t> variants = {
